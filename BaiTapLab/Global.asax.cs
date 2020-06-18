467f5b8c4ab7a53420d5f1b505b6281e7f1a9d0a
Nguyen Vo Cong Huy <conghuydt@gmail.com>
2020-06-18 10:48:49 +0700

    lam bai tai nha

--- BaiTapLab/Global.asax.cs
+++ BaiTapLab/Global.asax.cs
@@ -12,6 +12,7 @@ namespace BaiTapLab
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- BaiTapLab/Models/ApplicationUser.cs
+++ BaiTapLab/Models/ApplicationUser.cs
@@ -13,13 +13,22 @@ namespace BaiTapLab.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+
+        public ICollection<Following> Followers { get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+        public ApplicationUser()
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
         }
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        //{
+        //    // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+        //    var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        //    // Add custom user claims here
+        //    return userIdentity;
+        //}
     }
 
     
